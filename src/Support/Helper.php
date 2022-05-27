55f40a88c4e6bc74b3cdcf8dc52cd68ff1b92e86
mtvbrianking <mtvbrianking@gmail.com>
2022-05-27 17:08:53 +0300

    Fix filter domElements

--- src/Support/Helper.php
+++ src/Support/Helper.php
@@ -7,7 +7,17 @@ class Helper
     public static function getDomElements(\DOMNodeList $nodeList, ?string $nodeName): array
     {
         return array_filter(iterator_to_array($nodeList), function ($node) use ($nodeName) {
-            return $node instanceof \DOMElement && ($nodeName && $node->nodeName === $nodeName);
+            // return $node instanceof \DOMElement && ($nodeName && $node->nodeName === $nodeName);
+
+            if(! $node instanceof \DOMElement) {
+                return false;
+            }
+
+            if(! $nodeName) {
+                return true;
+            }
+
+            return $node->nodeName === $nodeName;
         });
     }
 }
