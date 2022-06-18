431d5725ed4d40bd5b1aaa32bacc4776071e217d
mtvbrianking <mtvbrianking@gmail.com>
2022-06-19 00:05:46 +0300

    fix array doc

--- src/Contracts/Aggregator.php
+++ src/Contracts/Aggregator.php
@@ -10,16 +10,16 @@ interface Aggregator
      * Call USSD service.
      *
      * @param string $uri
-     * @param array $data Input params from the simulator
-     *    $params = [
-     *        'new_session'   => (bool)    Request type
-     *        'session_id'    => (string)  Session ID - Auto generated
-     *        'phone_number'  => (string)  Phone Number
-     *        'service_code'  => (string)  Service Code
-     *        'network_code'  => (?string) Network Code
-     *        'input'         => (?string) Current user input
-     *        'answers'       => (?string) All the user's input for this session
-     *    ]
+     * @param array $params Input params from the simulator
+     *                      $params = [
+     *                          'new_session'   => (bool)    Request type
+     *                          'session_id'    => (string)  Session ID - Auto generated
+     *                          'phone_number'  => (string)  Phone Number
+     *                          'service_code'  => (string)  Service Code
+     *                          'network_code'  => (?string) Network Code
+     *                          'input'         => (?string) Current user input
+     *                          'answers'       => (?string) All the user's input for this session
+     *                      ]
      * @param OutputFormatterInterface $output
      *
      * @return string|null Successful response.
