943edb4e7e8fb09e89f7397540380a3d5f7b9542
mtvbrianking <mtvbrianking@gmail.com>
2024-04-19 14:24:05 +0300

    Decrement xpath

--- src/Traits/Expressions.php
+++ src/Traits/Expressions.php
@@ -4,6 +4,14 @@ namespace Bmatovu\Ussd\Traits;
 
 trait Expressions
 {
+    // Todo - handle decrement below position 1 edge case
+    protected function decExp(string $exp, int $step = 1): string
+    {
+        return preg_replace_callback('|(\\d+)(?!.*\\d)|', function ($matches) use ($step) {
+            return $matches[1] - $step;
+        }, $exp);
+    }
+
     protected function incExp(string $exp, int $step = 1): string
     {
         return preg_replace_callback('|(\\d+)(?!.*\\d)|', function ($matches) use ($step) {
