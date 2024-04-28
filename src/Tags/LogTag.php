7e7c0301fafde60ed5ba34f28e3440adb774cc9e
mtvbrianking <mtvbrianking@gmail.com>
2024-04-28 12:09:14 +0300

    Add log tag

---
+++ src/Tags/LogTag.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Bmatovu\Ussd\Tags;
+
+use Illuminate\Support\Facades\Log;
+
+/**
+ * ```xml
+ * <log message="{{alias}}" context="{{fname}},{{lname}}" />
+ * ```
+ */
+class LogTag extends BaseTag
+{
+    public function handle(): ?string
+    {
+        $level = $this->readAttr('level', 'debug');
+        $message = $this->readAttrText('message', '');
+        $context = $this->readAttr('context', '');
+
+        Log::{$level}($message, array_filter(explode(',', $context)));
+
+        $pre = $this->store->get('_pre');
+        $exp = $this->store->get('_exp', $this->node->getNodePath());
+
+        $this->store->put('_pre', $exp);
+        $this->store->put('_exp', $this->incExp($exp));
+
+        return '';
+    }
+}
